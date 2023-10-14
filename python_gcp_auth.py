300bf0b2ca235ba2f2aab50421fcd91a93fb65fb
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2023-10-14 23:33:19 +0900

    Update python_gcp_auth.py

--- python_gcp_auth.py
+++ python_gcp_auth.py
@@ -1,1 +1,19 @@
+# エンドユーザーとして認証する場合のコード
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
+json_file = "secret.json"
+
+appflow = InstalledAppFlow.from_client_secrets_file(
+    json_file,
+    scopes=['https://www.googleapis.com/auth/drive.file']
+)
+
+launch_browser = True
+
+if launch_browser:
+    appflow.run_local_server()
+else:
+    appflow.run_console()
+
+credentials = appflow.credentials
